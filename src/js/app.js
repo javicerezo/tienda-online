3b4fef4ea46c6256479213c0d47e7e192390f0aa
Javi Cerezo <jc.webmob@gmail.com>
2023-03-20 14:21:08 +0100

    maquetando BEMIT, js a medio hacer

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,18 @@
     // VARIABLES 
     // variables para eventos
     const btnBuscador = document.querySelector('.js-header__centro')
-    const btnConsulta = document.querySelector('.js-submenu__boton-consulta');
-    const btnChat = document.querySelector('.js-submenu__boton-chat');
+    const btnConsulta = document.querySelector('.js-submenus__boton-consulta');
+    const btnChat = document.querySelector('.js-submenus__boton-chat');
     const btnContacto = document.querySelector('.js-contacto');
     const formularioNewsletter = document.querySelector('.js-newsletter__form');
 
     // Variables contenedor para inyeccion dinámica de resultados
     const contenedorDestacados = document.querySelector('.js-item__grid');
     const contenedorNumArticulos = document.querySelector('.js-menus__cesta-numero');
-    const contenedorCarrito = document.querySelector('.js-menus__cesta');
-
-    let articulosCarrito = [];
+    const contenedorCarrito = document.querySelector('.js-submenus__cesta');
     
+    let articulosCarrito = [];
+
     // CLASES
     class UI {
         limpiarHTML (contenedor) {
@@ -28,25 +28,25 @@
                 imagen: producto.querySelector('img').src,
                 marca: producto.querySelector('.c-item__contenido-titulo:first-Child').textContent,
                 nombre: producto.querySelector('.c-item__contenido-titulo:last-Child').textContent,
-                precioNew: parseInt(producto.querySelector('.c-item__contenido-precio').textContent),
-                precio: parseInt(producto.querySelector('.c-item__contenido-precio--old').textContent),
-                descuento: parseInt(producto.querySelector('.c-item__descuento p').textContent),
+                precioNew: parseFloat(producto.querySelector('.c-item__contenido-precio').textContent),
+                precio: parseFloat(producto.querySelector('.c-item__contenido-precio--old').textContent),
+                descuento: parseFloat(producto.querySelector('.c-item__descuento p').textContent),
                 id: producto.querySelector('.c-button').getAttribute('data-id'),
                 cantidad: 1
-            };     
+            };    
             this.agregarCarrito(infoProducto);
         }
 
         agregarCarrito (producto) {
             // comprueba si existe en el producto y añade producto entero, o modifica su cantidad
-            const existe = articulosCarrito.some( art => art.id === producto.id );
+            const existe = articulosCarrito.some( articulo => articulo.id === producto.id );
             if (existe) { 
-                const articulosModificadosCarrito = articulosCarrito.map( art => {
-                    if (art.id === producto.id) {
-                        art.cantidad++;
-                        return art; //retorna obj modificado en cantidad
+                const articulosModificadosCarrito = articulosCarrito.map( articulo => {
+                    if (articulo.id === producto.id) {
+                        articulo.cantidad++;
+                        return articulo; //retorna obj modificado en cantidad
                     } else {
-                        return art; //retorna los demás obj no duplicados
+                        return articulo; //retorna los demás obj no duplicados
                     }
                 });
                 articulosCarrito = [... articulosModificadosCarrito];
@@ -57,44 +57,50 @@
             }
         }
 
+        eliminarArticulo (articulo) {
+            if (articulo.classList.contains('js-submenus__cesta-eliminar')) {
+                const id = articulo.getAttribute('data-id');
+                articulosCarrito = articulosCarrito.filter( a => a.id != id);
+                this.imprimircarrito(articulosCarrito);
+            }
+        }
+
         imprimircarrito (articulosCarrito){
-            console.log(articulosCarrito)
             this.limpiarHTML(contenedorCarrito);
             this.limpiarHTML(contenedorCarrito.parentElement.children[1]);
 
             // imprime los artículos del array articulosCarrito
             articulosCarrito.forEach( articulo => {
-                const {imagen, marca, nombre, precioNew, precio, descuento, cantidad} = articulo;
+                const {imagen, marca, nombre, precioNew, precio, descuento, cantidad, id} = articulo;
                 const li = document.createElement('li');
-                li.classList.add('c-submenus__secciones','c-submenus__secciones--mod3');
+                li.classList.add('c-submenus__cesta-secciones','c-submenus__cesta-secciones--mod2');
                 li.innerHTML = `
-                    <div class="c-submenus__img">
+                    <div class="c-submenus__cesta-img">
                         <img src="${imagen}" alt="imagen producto">
                     </div>
-                    <div class="c-submenus__contenido">
-                        <p class="c-submenus__nombre"><span>${marca}</span> - ${nombre}</p>
-                        <div class='c-submenus__precios'>
-                        <p><span>${precioNew} €</span></p>
-                        <p>(${descuento}%) - <span class="c-submenus__precios-old">${precio}€</span></p>
+                    <div class="c-submenus__cesta-contenido">
+                        <p><span class="c-submenus__cesta-span">${marca}</span> - ${nombre}</p>
+                        <div class='c-submenus__cesta-precios'>
+                            <p><span class="c-submenus__cesta-span">${precioNew} €</span></p>
+                            <p>(${descuento}%) - <span class="c-submenus__cesta-span--old">${precio}€</span></p>
                         </div>
-                        <p class="c-submenus__contenido-cantidad">Cantidad: ${cantidad} €<a href="#" class="c-submenus__eliminar">Eliminar</a></p>
+                        <p>Cantidad: ${cantidad} <a href="#" data-id=${id} class="c-submenus__cesta-eliminar js-submenus__cesta-eliminar">Eliminar</a></p>
                     </div>
                 `;
                 contenedorCarrito.appendChild(li);
                 if(descuento == 0) {
-                    console.log(' no tiene descuento')
+                    li.children[1].children[1].children[1].classList.add('u-display--none');
                 }
             });
-
-            // imprime el precio total de los articulos de articulosCarrito
             if(contenedorCarrito.childElementCount != 0) {
-                let sumaPrecios = articulosCarrito.reduce( (total, producto) => total += producto.cantidad*producto.precio, 0);
+                let sumaPrecios = articulosCarrito.reduce( (total, producto) => total += producto.cantidad*producto.precioNew, 0);
+                sumaPrecios = Math.round(sumaPrecios * 100)/100;
                 const total = document.createElement('div');
                 total.innerHTML = `
-                    <div class="c-submenus__separador">total: ${sumaPrecios}</div>
+                    <div class="c-submenus__separador">total: ${sumaPrecios} €</div>
                     <div class="c-submenus__resultado">
-                        <a href="#" class="c-submenus__ver-cesta">Ver la cesta</a>
-                        <button class="c-button c-button--amarillo">tramitar</button>
+                        <a href="#" class="c-submenus__ver-cesta js-submenus__ver-cesta">Ver la cesta</a>
+                        <button class="c-button c-button--amarillo c-submenus__resultado-button js-submenus__resultado-button">tramitar</button>
                     </div>
                 `; 
                 contenedorCarrito.parentElement.children[1].appendChild(total);   
@@ -129,7 +135,8 @@
             // destacados es un array en baseDatos.js
             destacados.forEach( d => {
                 const {marca, nombre, imagen, precio, descuento, id} = d;
-                const precioNew = precio * ((100-descuento)/100);
+                let precioNew = precio * ((100-descuento)/100);
+                precioNew = Math.round(precioNew *100)/100;
                 const articulo = document.createElement('div');                    
                 articulo.classList.add('c-item__articulo');
                 articulo.innerHTML = `                        
@@ -147,10 +154,11 @@
                         <div class ='c-item__contenido--mod'>
                             <p class='c-item__contenido-precio'>${precioNew} €</p>  
                             <p class='c-item__contenido-precio--old'>${precio} €</p>                        
+                        </div>
+                        <button class="c-button c-button--amarillo" data-id=${id}>Añadir a la cesta</button>                    
                     </div>
-                    <button class="c-button c-button--amarillo" data-id=${id}>Añadir a la cesta</button>
-                    </div>
-                `;
+                    <div class="c-item__mensaje"></div>
+                    `;
                 contenedorDestacados.appendChild(articulo);
             });
             // hago un repaso de los articulos sin descuento
@@ -160,12 +168,35 @@
                     contenedorDestacados.children[i].children[0].classList.add('u-display--none');
                     contenedorDestacados.children[i].children[2].children[1].children[1].classList.add('u-display--none');
                 }
-
             }
         }
         
-        imprimirAlerta () {
-            console.log('soy una alerta');
+        imprimirAlerta (lugar, tipo, mensaje) {
+            const p = document.createElement('p');
+            p.classList.add('u-mensaje');
+            p.textContent = mensaje;
+            if (tipo === 'exito') {
+                p.classList.add(`u-mensaje--${tipo}`);
+            }
+            if (tipo === 'error') {
+                p.classList.add(`u-mensaje--${tipo}`);
+            }
+            
+            lugar.parentElement.parentElement.children[3].appendChild(p);
+            setTimeout((e) => {
+                ui.limpiarHTML(lugar.parentElement.parentElement.children[3])
+            }, 3000);
+        }
+
+        verCesta (articulo) {
+            if (articulo.classList.contains('js-submenus__ver-cesta')) {
+                console.log('estas viendo la cesta')
+            }
+        }
+        tramitarPedido (articulo) {
+            if (articulo.classList.contains('js-submenus__resultado-button')) {
+                console.log('estas tramitando el pedido')
+            }
         }
     }
  
@@ -189,9 +220,18 @@
         contenedorDestacados.addEventListener('click', (e) => {
             if (e.target.classList.contains('c-button')){
                 const productoSeleccionado = e.target.parentElement.parentElement;
-                ui.leerArticulo(productoSeleccionado);            
+                ui.leerArticulo(productoSeleccionado); 
+                ui.imprimirAlerta(e.target,'exito', 'has añadido el producto al carrito');          
             }
         })
+        contenedorCarrito.addEventListener('click', (e) => {
+            ui.eliminarArticulo(e.target);
+        });
+
+        contenedorCarrito.parentElement.children[1].addEventListener('click', (e) => {
+            ui.verCesta(e.target);
+            ui.tramitarPedido(e.target);
+        });
     })
 
 
--- src/js/baseDatos.js
+++ src/js/baseDatos.js
@@ -1,92 +1,47 @@
 const destacados = [
     {
-        marca: 'marca',
-        nombre: 'producto 1',
+        marca: 'Marca',
+        nombre: 'Producto 1',
         imagen: 'build/img/destacados/destacados1.jpg',
-        precio: 50,
+        precio: 115.49,
         descuento: 0,
         sexo: 'h',
         id: 1
     },
     {
-        marca: 'marca',
-        nombre: 'producto 2',
+        marca: 'Marca',
+        nombre: 'Producto 2',
         imagen: 'build/img/destacados/destacados2.jpg',
-        precio: 50,
-        descuento: 10,
+        precio: 50.55,
+        descuento: 18,
         sexo: 'm',
         id: 2
     },
     {
-        marca: 'marca',
-        nombre: 'producto 3',
+        marca: 'Marca',
+        nombre: 'Producto 3',
         imagen: 'build/img/destacados/destacados3.jpg',
-        precio: 50,
-        descuento: 15,
+        precio: 322.80,
+        descuento: 21,
         sexo: 'u',
         id: 3
     },
     {
-        marca: 'marca',
-        nombre: 'producto 4',
+        marca: 'Marca',
+        nombre: 'Producto 4',
         imagen: 'build/img/destacados/destacados4.jpg',
-        precio: 50,
-        descuento: 50,
+        precio: 150.15,
+        descuento: 43,
         sexo: 'u',
         id: 4
     },
     {
-        marca: 'marca',
-        nombre: 'producto 5',
+        marca: 'Marca',
+        nombre: 'Producto 5',
         imagen: 'build/img/destacados/destacados5.jpg',
-        precio: 50,
+        precio: 80.45,
         descuento: 0,
         sexo: 'u',
         id: 5
-    },
-    {
-        marca: 'marca',
-        nombre: 'producto 5',
-        imagen: 'build/img/destacados/destacados5.jpg',
-        precio: 50,
-        descuento: 0,
-        sexo: 'u',
-        id: 5
-    },
-    {
-        marca: 'marca',
-        nombre: 'producto 5',
-        imagen: 'build/img/destacados/destacados5.jpg',
-        precio: 50,
-        descuento: 0,
-        sexo: 'u',
-        id: 5
-    }, 
-    {
-        marca: 'marca',
-        nombre: 'producto 1',
-        imagen: 'build/img/destacados/destacados1.jpg',
-        precio: 50,
-        descuento: 0,
-        sexo: 'h',
-        id: 1
-    },
-    {
-        marca: 'marca',
-        nombre: 'producto 1',
-        imagen: 'build/img/destacados/destacados1.jpg',
-        precio: 50,
-        descuento: 0,
-        sexo: 'h',
-        id: 1
-    },
-    {
-        marca: 'marca',
-        nombre: 'producto 1',
-        imagen: 'build/img/destacados/destacados1.jpg',
-        precio: 50,
-        descuento: 0,
-        sexo: 'h',
-        id: 1
     }
 ];
